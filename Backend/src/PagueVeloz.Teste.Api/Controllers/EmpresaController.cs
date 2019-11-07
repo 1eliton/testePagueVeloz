3bbc364e1ca95216d9ca80d850a64eec5072f3a4
elitonlunardi <lunardi.eliton@gmail.com>
2019-11-06 21:46:06 -0300

    adicionado apibaseController e empresaController

---
+++ Backend/src/PagueVeloz.Teste.Api/Controllers/EmpresaController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PagueVeloz.Teste.Application;
+using PagueVeloz.Teste.Application.DTOs;
+using PagueVeloz.Teste.Domain.Core;
+
+namespace PagueVeloz.Teste.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ApiBaseController
+    {
+        private readonly IEmpresaAppService _empresaAppService;
+
+        public EmpresaController(INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediator,
+            IEmpresaAppService empresaAppService)
+            : base(notifications, mediator)
+        {
+            _empresaAppService = empresaAppService;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(CadastrarEmpresaDto empresaDto)
+        {
+            _empresaAppService.Cadastar(empresaDto);
+            return Response();
+        }
+    }
+}
